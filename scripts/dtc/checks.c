0b962816b08b427578c139c4e8e4c3cea3d8e7ce
Davide Gasparetto <91277246+spacefall@users.noreply.github.com>
2026-05-31 20:05:47 +0000

    dtc: fix segfault in build
    asked gpt for this, sorry

--- scripts/dtc/checks.c
+++ scripts/dtc/checks.c
@@ -241,7 +241,7 @@ static void check_is_cell(struct check *c, struct dt_info *dti,
 #define ERROR_IF_NOT_CELL(nm, propname) \
 	ERROR(nm, check_is_cell, (propname))
 
-static void check_is_null_terminated(struct check *c, struct node *root,
+static void check_is_null_terminated(struct check *c, struct dt_info *dti,
 			    struct node *node)
 {
 	struct property *prop;
@@ -252,13 +252,14 @@ static void check_is_null_terminated(struct check *c, struct node *root,
 		return; /* Not present, assumed ok */
 
 	if (prop->val.val[prop->val.len-1] != '\0')
-		FAIL(c, "\"%s\" property in %s is not null terminated",
-		     propname, node->fullpath);
+		FAIL_PROP(c, dti, node, prop,
+			  "\"%s\" property in %s is not null terminated",
+			  propname, node->fullpath);
 }
 #define WARNING_IF_NOT_NULL_TERMINATED(nm, propname) \
-	WARNING(nm, NULL, check_is_null_terminated, NULL, (propname))
+	WARNING(nm, check_is_null_terminated, (propname))
 #define ERROR_IF_NOT_NULL_TERMINATED(nm, propname) \
-	ERROR(nm, NULL, check_is_null_terminated, NULL, (propname))
+	ERROR(nm, check_is_null_terminated, (propname))
 
 /*
  * Structural check functions

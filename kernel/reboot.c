3bd150927638efbaa12f20cac59d45892c6a3d4b
spacefall <lucarichu@outlook.it>
2026-05-31 23:13:29 +0200

    kernelsu: scope-minimized manual hooks
    integrated using syscall_hook_patches.sh
    https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd/blob/b4a85639f4e0965424d0146330141fe77c39058c/Patches/syscall_hook_patches.sh

--- kernel/reboot.c
+++ kernel/reboot.c
@@ -305,6 +305,10 @@ DEFINE_MUTEX(system_transition_mutex);
  *
  * reboot doesn't sync: do that yourself before calling this.
  */
+#ifdef CONFIG_KSU
+extern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);
+#endif
+
 SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 		void __user *, arg)
 {
@@ -313,6 +317,12 @@ SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 	int ret = 0;
 
 	/* We only trust the superuser with rebooting the system. */
+#ifdef CONFIG_KSU
+	if (system_state == SYSTEM_RUNNING) {
+		ksu_handle_sys_reboot(magic1, magic2, cmd, &arg);
+	}
+#endif
+
 	if (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))
 		return -EPERM;
 
--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -6306,6 +6306,10 @@ bad:
 	return error;
 }
 
+#ifdef CONFIG_KSU
+extern int ksu_hide_setprocattr(const char *name, void *value, size_t size);
+#endif
+
 static int selinux_setprocattr(const char *name, void *value, size_t size)
 {
 	struct task_security_struct *tsec;
@@ -6313,6 +6317,10 @@ static int selinux_setprocattr(const char *name, void *value, size_t size)
 	u32 mysid = current_sid(), sid = 0, ptsid;
 	int error;
 	char *str = value;
+#ifdef CONFIG_KSU
+    ksu_hide_setprocattr(name, value, size);
+#endif
+
 
 	/*
 	 * Basic control over ability to set these attributes at all.

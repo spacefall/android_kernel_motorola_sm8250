09012df268654f13895c00abeab51e5f9713ba7a
spacefall <lucarichu@outlook.it>
2026-06-02 17:40:33 +0200

    idek

--- build.sh
+++ build.sh
@@ -8,9 +8,20 @@ CONFIGS=(
     "vendor/lito-perf_defconfig"
     "vendor/ext_config/moto-lito.config"
     "vendor/ext_config/nairo-default.config"
+    "vendor/debugfs.config"
+    "ksu.config"
+    "docker.config"
+    "droidspaces.config"
+    "droidspaces-additional.config"
+    "additional.config"
+    "lto.config"
 )
 ADDITIONAL_BUILD_FLAGS=(
     "DTC_FLAGS=-f"
+    "LLVM_IAS=1"
+    "CLANG_TRIPLE=aarch64-linux-gnu-"
+    "CROSS_COMPILE=aarch64-linux-android-"
+    "CROSS_COMPILE_COMPAT=arm-linux-androidkernel-"
     "Image.gz-dtb"
 )
 
@@ -110,7 +121,7 @@ if [ ! -d "AnyKernel" ]; then
     git clone "$AK3_REPO" "AnyKernel" --depth=1 --branch=$DEVICE
 fi
 
-perform_clean
+#perform_clean
 
 build_kernel
 echo "🎉 Build for $DEVICE is complete."
